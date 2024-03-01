3a03e59dde8efd46b372ae8ac16dbaec12a10f80
Lucas M <lucasm676@gmail.com>
2024-03-01 19:22:13 -0300

    🚧: Criado o modelo para o treinador e o pokemon

--- apps/desafio/models.py
+++ apps/desafio/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
+
+
+class Treinador(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Pokemon(models.Model):
+    treinador = models.ForeignKey(Treinador, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    ataque_1 = models.CharField(max_length=100)
+    ataque_2 = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome

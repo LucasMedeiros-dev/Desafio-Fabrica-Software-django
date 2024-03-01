39cb18b713905a6cdf9f9724aa339150c8268ce4
Lucas M <lucasm676@gmail.com>
2024-03-01 19:07:45 -0300

    🔧 chore: Configurado o DRF

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Adicionado o rest_framework
     'apps.desafio',  # Adicionado o aplicativo desafio
 ]
 

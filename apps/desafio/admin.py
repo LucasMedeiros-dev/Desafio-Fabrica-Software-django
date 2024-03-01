3f2c56c44a304d3e0581642ba86bbbac48c7b52a
Lucas M <lucasm676@gmail.com>
2024-03-01 20:50:06 -0300

    :construction: Criado o admin para os modelos :books: docs: documentadas as possibilidades

--- apps/desafio/admin.py
+++ apps/desafio/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+# Importa os modelos Treinador e Pokemon que estão no mesmo nível de diretório (.)
+from .models import Treinador, Pokemon
 # Register your models here.
+
+# Registra os modelos Treinador e Pokemon no admin
+
+
+@admin.register(Treinador)
+class TreinadorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    pass
+
+# Ou
+
+# admin.site.register(Treinador)
+# admin.site.register(Pokemon)

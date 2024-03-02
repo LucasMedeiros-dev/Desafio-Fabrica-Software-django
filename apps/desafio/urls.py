34029b3ab52f644c25ff2188d9b12e07543f2b84
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-03-02 00:23:02 -0300

    feat: urls para acesso das views de treinador e pokemons

--- apps/desafio/urls.py
+++ apps/desafio/urls.py
@@ -1,9 +1,34 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'desafio'
+app_name = "desafio"
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('api/', include('apps.desafio.api.urls')),
+    path("api/", include("apps.desafio.api.urls")),
+    path(
+        "app/criar_treinador/",
+        views.TreinadorCreateView.as_view(),
+        name="criar_treinador",
+    ),
+    path(
+        "app/ver_treinador/<int:pk>",
+        views.TreinadorDetailView.as_view(),
+        name="ver_treinador",
+    ),
+    path(
+        "app/atualizar_treinador/<int:pk>",
+        views.TreinadorUpdateView.as_view(),
+        name="atualizar_treinador",
+    ),
+    path(
+        "app/criar_pokemon/",
+        views.PokemonCreateView.as_view(),
+        name="criar_pokemon",
+    ),
+    path(
+        "app/atualizar_pokemon/<int:pk>",
+        views.PokemonUpdateView.as_view(),
+        name="atualizar_pokemon",
+    ),
 ]

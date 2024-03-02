a8c75f7bd25de73208519147784ab5e578a3d2b6
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-03-02 00:22:41 -0300

    feat: views para treinador (create, update e read) e para pokemon

--- apps/desafio/views.py
+++ apps/desafio/views.py
@@ -1,4 +1,45 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView
 from django.contrib.auth.views import LoginView, LogoutView
-# Create your views here.
+
+from .forms import TreinadorForm, PokemonForm
+from .models import Treinador, Pokemon
+
+
+class TreinadorCreateView(CreateView):
+    model = Treinador
+    template_name = "desafio/treinador_form.html"
+    form_class = TreinadorForm
+    # success_url = reverse_lazy("desafio:ver_treinador")
+
+    def get_success_url(self) -> str:
+        return reverse_lazy(f"desafio:ver_treinador", kwargs={"pk": self.object.pk})
+
+
+class TreinadorUpdateView(UpdateView):
+    model = Treinador
+    template_name = "desafio/treinador_form.html"
+    form_class = TreinadorForm
+    success_url = "/"
+
+
+class TreinadorDetailView(DetailView):
+    model = Treinador
+    template_name = "desafio/detalhes_treinador.html"
+    context_object_name = "treinador"
+    success_url = "/"
+
+
+class PokemonCreateView(CreateView):
+    model = Pokemon
+    template_name = "desafio/pokemon_form.html"
+    form_class = PokemonForm
+    success_url = "/"
+
+
+class PokemonUpdateView(UpdateView):
+    model = Pokemon
+    template_name = "desafio/pokemon_form.html"
+    form_class = PokemonForm
+    success_url = "/"

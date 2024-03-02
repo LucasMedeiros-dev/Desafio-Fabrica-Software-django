3a1fcb3fe024585a3a0b54394b082b8c58066a1b
Lucas M <lucasm676@gmail.com>
2024-03-01 23:33:38 -0300

    :construction: Criada a view de criar usário e o html de criar conta

--- apps/desafio/views.py
+++ apps/desafio/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 from django.views.generic import CreateView, DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 # Create your views here.
+
+# View Para Criação de Conta.
+
+
+class CriarContaView(CreateView):
+    template_name = 'criar_conta.html'
+    success_url = reverse_lazy('desafio:treinador')
+    # Define o modelo que será utilizado
+    model = User
+    # Define os campos que serão utilizados
+    fields = ['username', 'password']
